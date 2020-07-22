bd6ea0cefa250bb27c19144fda6626dc353106c4
nghiahsgs <nghiahsgs@gmail.com>
2020-07-22 02:54:02 +0000

    make it better

--- index.py
+++ index.py
@@ -1,14 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-CHROMEDRIVER_PATH = '/usr/local/bin/chromedriver'
-WINDOW_SIZE = "1920,1080"
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
-chrome_options.add_argument('--no-sandbox')
-driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,
-                          chrome_options=chrome_options
-                         )
-driver.get("https://www.google.com")
-print(driver.title)
+
+def initDriver():
+    CHROMEDRIVER_PATH = '/usr/local/bin/chromedriver'
+    WINDOW_SIZE = "1920,1080"
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+    chrome_options.add_argument('--no-sandbox')
+    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,
+                            chrome_options=chrome_options
+                            )
+    return driver
+
+driver=initDriver()
+driver.get("http://nghiahsgs.com/vps")
+text=driver.find_element_by_css_selector('body').text
+print(text)
+
+driver.get_screenshot_as_file('test.png')
 driver.close()
+
+
